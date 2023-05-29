9ac1c98522f1128b23c79497ae4dbddc43913fff
AnastasiaFalk <125199635+AnastasiaFalk@users.noreply.github.com>
2023-05-29 19:01:03 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,39 +1,49 @@
-const firstName = document.getElementById('firstname').value;
-const secondName = document.getElementById('secondname').value;
-const phone = document.getElementById('numberphone').value;
-const typeMassage = document.getElementById('type-massage').value;
-const date = document.getElementById('date').value;
+const form = document.getElementById('contact-form');
+const buttonSubmit = document.getElementById('send');
+buttonSubmit.addEventListener('click', () => {
 
-const token = "токен";
-const chatId = 'chat_id’;
-const spaces = '=======================';
-let txt = '';
+  const email = document.getElementById('email').value;
+  const phone = document.getElementById('phone').value;
+  const message = document.getElementById('message').value;
 
-const data = {
-  'Номер телефону: ': phone,
-  'e-mail: ': email,
-  'Повідомлення: ': message,
-};
 
-txt += 'Новий клієнт %0A%0A' + spaces + '%0A%0A';
+  const token = "6046445208:AAF7SZ1pmkKYRgGHAGzbdfhHmfSC7bpes5g";
+  const chatId = '-1001910885603';
 
-for (const [key, value] of Object.entries(data)) {
-  txt += '<b>' + key + '</b> ' + value + '%0A';
-}
+  const spaces = '=======================';
+  let txt = '';
 
-txt += '%0A' + spaces;
+  const dataSend = {
+    'Номер телефону: ': phone,
+    'Email: ': email,
+    'Повідомлення: ': message,
+  };
 
-fetch(`https://api.telegram.org/bot${6046445208:AAF7SZ1pmkKYRgGHAGzbdfhHmfSC7bpes5g}/sendMessage?chat_id=${-1001910885603}&parse_mode=html&text=${txt}`)
-  .then(response => {
-    if (response.ok) {
-      // Виконано успішний запит
-      console.log('Повідомлення надіслано');
-    } else {
-      // Обробка помилки відповіді
-      console.log('Помилка при відправленні повідомлення');
-    }
-  })
-  .catch(error => {
-    // Обробка помилки з'єднання
-    console.log('Помилка з\'єднання:', error);
-  });
+  txt += 'Новий клієнт %0A%0A' + spaces + '%0A%0A';
+
+  for (const [key, value] of Object.entries(dataSend)) {
+    txt += '<b>' + key + '</b> ' + value + '%0A';
+  }
+
+  txt += '%0A' + spaces;
+
+  fetch(`https://api.telegram.org/bot${token}/sendMessage?chat_id=${chatId}&parse_mode=html&text=${txt}`)
+    .then(response => {
+      if (response.ok) {
+        // Виконано успішний запит
+        console.log('Повідомлення надіслано');
+        const result = document.createElement('div');
+        result.classList = 'result';
+        result.innerText = 'Повідомлення надіслано';
+        form.appendChild(result);
+      } else {
+        // Обробка помилки відповіді
+        console.log('Помилка при відправленні повідомлення');
+      }
+    })
+    .catch(error => {
+      // Обробка помилки з'єднання
+      console.log('Помилка з\'єднання:', error);
+    });
+
+})
